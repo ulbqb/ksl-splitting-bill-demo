d35e04312b7f443c9f7a0d9fa0a33ad7f2b51ff3
Shogo Hyodo <mmoshg8u@gmail.com>
2024-08-27 13:06:27 +0900

    Update UI

--- app/page.tsx
+++ app/page.tsx
@@ -69,7 +69,7 @@ const Form = (props: PrepareProps) => {
   };
   return (
     <>
-      <h1 className="text-3xl font-bold">{amount} KAIA + Fee</h1>
+      <h1 className="text-3xl font-bold">{amount} KAIA</h1>
       <button
         type="button"
         className="w-64 h-14 bg-kaia text-black text-bg leading-6 font-medium py-2 px-3 rounded-lg inline-flex items-center"
@@ -105,7 +105,7 @@ function Generated(props: GeneratedProps) {
   const shareLink = `${window.location.protocol}//${
     window.location.host
   }/receive/?k=${base64url.encode(props.privateKey)}`;
-  const sendToFriend = () => {
+  const sendToKakaoFriend = () => {
     const content = {
       objectType: "text",
       text: `I sent ${props.amount} KAIA.`,
@@ -124,6 +124,12 @@ function Generated(props: GeneratedProps) {
     }
     _Kakao.Share.sendDefault(content);
   };
+  const sendToLINEFriend = () => {
+    const encodedMsg = encodeURIComponent(
+      `I sent ${props.amount} KAIA.Please open the following link to receive KAIA.\n\n${shareLink}`
+    );
+    location.href = `line://share?text=${encodedMsg}`;
+  };
   return (
     <>
       <div></div>
@@ -132,26 +138,48 @@ function Generated(props: GeneratedProps) {
       <h1 className="w-64 text-base font-medium truncate text-white/75">
         {shareLink}
       </h1>
-      <button
-        type="button"
-        className="w-64 h-14 bg-kakao text-black text-bg leading-6 font-medium py-2 px-3 rounded-lg inline-flex items-center"
-        onClick={sendToFriend}
-      >
-        <svg
-          className="ms-2 me-7"
-          width="22"
-          height="22"
-          viewBox="0 0 512 512"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
+      <div>
+        <button
+          type="button"
+          className="w-64 h-14 bg-kakao text-black text-bg leading-6 font-medium py-2 px-3 rounded-lg inline-flex items-center mb-5"
+          onClick={sendToKakaoFriend}
         >
-          <path
-            fill="#000000"
-            d="M255.5 48C299.345 48 339.897 56.5332 377.156 73.5996C414.415 90.666 443.871 113.873 465.522 143.22C487.174 172.566 498 204.577 498 239.252C498 273.926 487.174 305.982 465.522 335.42C443.871 364.857 414.46 388.109 377.291 405.175C340.122 422.241 299.525 430.775 255.5 430.775C241.607 430.775 227.262 429.781 212.467 427.795C148.233 472.402 114.042 494.977 109.892 495.518C107.907 496.241 106.012 496.15 104.208 495.248C103.486 494.706 102.945 493.983 102.584 493.08C102.223 492.177 102.043 491.365 102.043 490.642V489.559C103.126 482.515 111.335 453.169 126.672 401.518C91.8486 384.181 64.1974 361.2 43.7185 332.575C23.2395 303.951 13 272.843 13 239.252C13 204.577 23.8259 172.566 45.4777 143.22C67.1295 113.873 96.5849 90.666 133.844 73.5996C171.103 56.5332 211.655 48 255.5 48Z"
-          ></path>
-        </svg>
-        Send with Kakao
-      </button>
+          <svg
+            className="ms-2 me-7"
+            width="22"
+            height="22"
+            viewBox="0 0 512 512"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="#000000"
+              d="M255.5 48C299.345 48 339.897 56.5332 377.156 73.5996C414.415 90.666 443.871 113.873 465.522 143.22C487.174 172.566 498 204.577 498 239.252C498 273.926 487.174 305.982 465.522 335.42C443.871 364.857 414.46 388.109 377.291 405.175C340.122 422.241 299.525 430.775 255.5 430.775C241.607 430.775 227.262 429.781 212.467 427.795C148.233 472.402 114.042 494.977 109.892 495.518C107.907 496.241 106.012 496.15 104.208 495.248C103.486 494.706 102.945 493.983 102.584 493.08C102.223 492.177 102.043 491.365 102.043 490.642V489.559C103.126 482.515 111.335 453.169 126.672 401.518C91.8486 384.181 64.1974 361.2 43.7185 332.575C23.2395 303.951 13 272.843 13 239.252C13 204.577 23.8259 172.566 45.4777 143.22C67.1295 113.873 96.5849 90.666 133.844 73.5996C171.103 56.5332 211.655 48 255.5 48Z"
+            ></path>
+          </svg>
+          Send with Kakao
+        </button>
+        <button
+          type="button"
+          className="w-64 h-14 bg-line text-white text-bg leading-6 font-medium py-2 px-3 rounded-lg inline-flex items-center"
+          onClick={sendToLINEFriend}
+        >
+          <svg
+            className="ms-2 me-7"
+            width="22"
+            height="22"
+            viewBox="0 0 512 512"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="#ffffff"
+              d="M255.5 48C299.345 48 339.897 56.5332 377.156 73.5996C414.415 90.666 443.871 113.873 465.522 143.22C487.174 172.566 498 204.577 498 239.252C498 273.926 487.174 305.982 465.522 335.42C443.871 364.857 414.46 388.109 377.291 405.175C340.122 422.241 299.525 430.775 255.5 430.775C241.607 430.775 227.262 429.781 212.467 427.795C148.233 472.402 114.042 494.977 109.892 495.518C107.907 496.241 106.012 496.15 104.208 495.248C103.486 494.706 102.945 493.983 102.584 493.08C102.223 492.177 102.043 491.365 102.043 490.642V489.559C103.126 482.515 111.335 453.169 126.672 401.518C91.8486 384.181 64.1974 361.2 43.7185 332.575C23.2395 303.951 13 272.843 13 239.252C13 204.577 23.8259 172.566 45.4777 143.22C67.1295 113.873 96.5849 90.666 133.844 73.5996C171.103 56.5332 211.655 48 255.5 48Z"
+            ></path>
+          </svg>
+          Send with LINE
+        </button>
+      </div>
       <div></div>
       <div></div>
     </>
--- app/success/page.tsx
+++ app/success/page.tsx
@@ -21,34 +21,23 @@ const Form = () => {
     init();
   }, []);
 
-  const shareThanksMessage = async () => {
+  const thanksToKakaoFriend = async () => {
     try {
-      const content = {
-        objectType: "text",
-        text: "KAIA has been received. Thank you :)",
-        link: {
-          webUrl: "https://developers.kakao.com",
-          mobileWebUrl: "https://ksl-splitting-bill-demo.vercel.app",
-        },
-        buttons: [
-          {
-            title: "Check Transaction",
-            link: {
-              webUrl: "https://developers.kakao.com",
-              mobileWebUrl: "https://ksl-splitting-bill-demo.vercel.app",
-            },
-          },
-        ],
-      };
       // @ts-ignore
       const _Kakao = Kakao;
       if (!_Kakao.isInitialized()) {
         _Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
       }
-      _Kakao.Share.sendDefault(content);
+      _Kakao.Share.sendCustom({
+        templateId: 111545,
+        templateArgs: {},
+      });
     } catch (e) {
       console.error(e);
     }
+  };
+  const thanksToLINEFriend = () => {
+    location.href = `line://share?text=KAIA+has+been+received.+Thank+you+%3A%29`;
   };
 
   return (
@@ -58,26 +47,48 @@ const Form = () => {
         <br></br>
         <h1 className="text-3xl font-bold">{amount} KAIA</h1>
       </div>
-      <button
-        type="button"
-        className="w-64 h-14 bg-kakao text-black text-bg leading-6 font-medium py-2 px-3 rounded-lg inline-flex items-center"
-        onClick={shareThanksMessage}
-      >
-        <svg
-          className="ms-2 me-7"
-          width="22"
-          height="22"
-          viewBox="0 0 512 512"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
+      <div>
+        <button
+          type="button"
+          className="w-64 h-14 bg-kakao text-black text-bg leading-6 font-medium py-2 px-3 rounded-lg inline-flex items-center mb-5"
+          onClick={thanksToKakaoFriend}
         >
-          <path
-            fill="#000000"
-            d="M255.5 48C299.345 48 339.897 56.5332 377.156 73.5996C414.415 90.666 443.871 113.873 465.522 143.22C487.174 172.566 498 204.577 498 239.252C498 273.926 487.174 305.982 465.522 335.42C443.871 364.857 414.46 388.109 377.291 405.175C340.122 422.241 299.525 430.775 255.5 430.775C241.607 430.775 227.262 429.781 212.467 427.795C148.233 472.402 114.042 494.977 109.892 495.518C107.907 496.241 106.012 496.15 104.208 495.248C103.486 494.706 102.945 493.983 102.584 493.08C102.223 492.177 102.043 491.365 102.043 490.642V489.559C103.126 482.515 111.335 453.169 126.672 401.518C91.8486 384.181 64.1974 361.2 43.7185 332.575C23.2395 303.951 13 272.843 13 239.252C13 204.577 23.8259 172.566 45.4777 143.22C67.1295 113.873 96.5849 90.666 133.844 73.5996C171.103 56.5332 211.655 48 255.5 48Z"
-          ></path>
-        </svg>
-        Thanks with Kakao
-      </button>
+          <svg
+            className="ms-2 me-7"
+            width="22"
+            height="22"
+            viewBox="0 0 512 512"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="#000000"
+              d="M255.5 48C299.345 48 339.897 56.5332 377.156 73.5996C414.415 90.666 443.871 113.873 465.522 143.22C487.174 172.566 498 204.577 498 239.252C498 273.926 487.174 305.982 465.522 335.42C443.871 364.857 414.46 388.109 377.291 405.175C340.122 422.241 299.525 430.775 255.5 430.775C241.607 430.775 227.262 429.781 212.467 427.795C148.233 472.402 114.042 494.977 109.892 495.518C107.907 496.241 106.012 496.15 104.208 495.248C103.486 494.706 102.945 493.983 102.584 493.08C102.223 492.177 102.043 491.365 102.043 490.642V489.559C103.126 482.515 111.335 453.169 126.672 401.518C91.8486 384.181 64.1974 361.2 43.7185 332.575C23.2395 303.951 13 272.843 13 239.252C13 204.577 23.8259 172.566 45.4777 143.22C67.1295 113.873 96.5849 90.666 133.844 73.5996C171.103 56.5332 211.655 48 255.5 48Z"
+            ></path>
+          </svg>
+          Thanks with Kakao
+        </button>
+        <button
+          type="button"
+          className="w-64 h-14 bg-line text-white text-bg leading-6 font-medium py-2 px-3 rounded-lg inline-flex items-center"
+          onClick={thanksToLINEFriend}
+        >
+          <svg
+            className="ms-2 me-7"
+            width="22"
+            height="22"
+            viewBox="0 0 512 512"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="#ffffff"
+              d="M255.5 48C299.345 48 339.897 56.5332 377.156 73.5996C414.415 90.666 443.871 113.873 465.522 143.22C487.174 172.566 498 204.577 498 239.252C498 273.926 487.174 305.982 465.522 335.42C443.871 364.857 414.46 388.109 377.291 405.175C340.122 422.241 299.525 430.775 255.5 430.775C241.607 430.775 227.262 429.781 212.467 427.795C148.233 472.402 114.042 494.977 109.892 495.518C107.907 496.241 106.012 496.15 104.208 495.248C103.486 494.706 102.945 493.983 102.584 493.08C102.223 492.177 102.043 491.365 102.043 490.642V489.559C103.126 482.515 111.335 453.169 126.672 401.518C91.8486 384.181 64.1974 361.2 43.7185 332.575C23.2395 303.951 13 272.843 13 239.252C13 204.577 23.8259 172.566 45.4777 143.22C67.1295 113.873 96.5849 90.666 133.844 73.5996C171.103 56.5332 211.655 48 255.5 48Z"
+            ></path>
+          </svg>
+          Thanks with LINE
+        </button>
+      </div>
     </>
   );
 };
